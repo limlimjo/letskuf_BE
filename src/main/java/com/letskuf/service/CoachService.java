36e4d3f6d2507c09f1c28c9eeb46751fd291ef29
limlimjo <skyblue.777@daum.net>
2026-08-09 22:02:54 +0900

    fix: 선수단 관리 등록/수정 파일 부분 제거

--- src/main/java/com/letskuf/service/CoachService.java
+++ src/main/java/com/letskuf/service/CoachService.java
@@ -31,48 +31,50 @@ public class CoachService {
     /* 코칭스태프/임원 등록 */
     @Transactional(rollbackFor = Exception.class) // 모든 예외 발생 시 롤백
     public void registerCoach(CoachDTO coachDTO) throws IOException {
-        // 파일이 없을 때
-        if (coachDTO.getFile().get(0).isEmpty()) {
-            coachDTO.setFileAttached(0);
-            coachRepository.save(coachDTO);
-            log.info("지도자 정보 저장(파일x): {}", coachDTO);
-        }
-        // 파일이 있을 때
-        else {
-            coachDTO.setFileAttached(1);
-            CoachDTO savedCoach = coachRepository.save(coachDTO);
-            log.info("지도자 정보 저장(파일o): {}", coachDTO);
-
-            for (MultipartFile coachFile: coachDTO.getFile()) {
-                String originalFileName = coachFile.getOriginalFilename();
-                String storedFileName = System.currentTimeMillis() + "-" + originalFileName;
-
-                CoachFileDTO coachFileDTO = new CoachFileDTO();
-                coachFileDTO.setOriginalFileName(originalFileName);
-                coachFileDTO.setStoredFileName(storedFileName);
-                coachFileDTO.setCoachId(savedCoach.getCoachId());
-                // 파일 저장용 폴더에 파일 저장 처리
-                String savePath = "/Users/selimjo/Desktop/letskuf_pic/coach/";
-                String storedFilePath = savePath + storedFileName;
-
-                // 파일 저장용 폴더 없는 경우 폴더 생성
-                Path directoryPath = Paths.get(savePath);
-                if (!Files.exists(directoryPath)) {
-                    try {
-                        Files.createDirectories(directoryPath);
-                        log.info("폴더 생성 완료: " + savePath);
-                    } catch (IOException e) {
-                        log.error("파일 저장 폴더 생성 실패: " + savePath, e);
-                        throw new IOException("파일 저장 폴더 생성에 실패하였습니다.", e);
-                    }
-                }
-
-                coachFile.transferTo(new File(storedFilePath));
-
-                // coach_file 테이블에 저장
-                coachRepository.saveFile(coachFileDTO);
-            }
-        }
+        // coach 저장
+        coachRepository.save(coachDTO);
+//        // 파일이 없을 때
+//        if (coachDTO.getFile().get(0).isEmpty()) {
+//            coachDTO.setFileAttached(0);
+//            coachRepository.save(coachDTO);
+//            log.info("지도자 정보 저장(파일x): {}", coachDTO);
+//        }
+//        // 파일이 있을 때
+//        else {
+//            coachDTO.setFileAttached(1);
+//            CoachDTO savedCoach = coachRepository.save(coachDTO);
+//            log.info("지도자 정보 저장(파일o): {}", coachDTO);
+//
+//            for (MultipartFile coachFile: coachDTO.getFile()) {
+//                String originalFileName = coachFile.getOriginalFilename();
+//                String storedFileName = System.currentTimeMillis() + "-" + originalFileName;
+//
+//                CoachFileDTO coachFileDTO = new CoachFileDTO();
+//                coachFileDTO.setOriginalFileName(originalFileName);
+//                coachFileDTO.setStoredFileName(storedFileName);
+//                coachFileDTO.setCoachId(savedCoach.getCoachId());
+//                // 파일 저장용 폴더에 파일 저장 처리
+//                String savePath = "/Users/selimjo/Desktop/letskuf_pic/coach/";
+//                String storedFilePath = savePath + storedFileName;
+//
+//                // 파일 저장용 폴더 없는 경우 폴더 생성
+//                Path directoryPath = Paths.get(savePath);
+//                if (!Files.exists(directoryPath)) {
+//                    try {
+//                        Files.createDirectories(directoryPath);
+//                        log.info("폴더 생성 완료: " + savePath);
+//                    } catch (IOException e) {
+//                        log.error("파일 저장 폴더 생성 실패: " + savePath, e);
+//                        throw new IOException("파일 저장 폴더 생성에 실패하였습니다.", e);
+//                    }
+//                }
+//
+//                coachFile.transferTo(new File(storedFilePath));
+//
+//                // coach_file 테이블에 저장
+//                coachRepository.saveFile(coachFileDTO);
+//            }
+//        }
     }
 
     /* 코칭스태프/임원 수정 */
@@ -86,50 +88,50 @@ public class CoachService {
         }
 
         // 파일이 새로 업로드 되었는지 확인
-        List<MultipartFile> newFiles = coachDTO.getFile();
-        String savePath = "/Users/selimjo/Desktop/letskuf_pic/player/";
-
-        // 파일 변경 있을 경우
-        if (newFiles != null && !newFiles.isEmpty() && !newFiles.get(0).isEmpty()) {
-            // 기존 파일이 있다면 삭제 처리
-            List<CoachFileDTO> existingFiles = coachRepository.selectCoachFileListByListByCoachId(coachDTO.getCoachId());
-
-            for (CoachFileDTO file : existingFiles) {
-                File storedFile = new File(savePath + file.getStoredFileName());
-                if (storedFile.exists()) {
-                    storedFile.delete();
-                }
-                log.info("파일 변경있을 경우 삭제 되어야함");
-                coachRepository.deleteFile(file.getFileId());
-            }
-
-            // 새 파일 저장
-            coachDTO.setFileAttached(1);
-            for (MultipartFile coachFile: newFiles) {
-                String originalFileName = coachFile.getOriginalFilename();
-                String storedFileName = System.currentTimeMillis() + "-" + originalFileName;
-                String storedFilePath = savePath + storedFileName;
-
-                // 저장 폴더 없으면 생성
-                Path directoryPath = Paths.get(savePath);
-                if (!Files.exists(directoryPath)) {
-                    Files.createDirectories(directoryPath);
-                }
-
-                coachFile.transferTo(new File(storedFilePath));
-
-                CoachFileDTO newFileDTO = new CoachFileDTO();
-                newFileDTO.setOriginalFileName(originalFileName);
-                newFileDTO.setStoredFileName(storedFileName);
-                newFileDTO.setCoachId(coachDTO.getCoachId());
-
-                coachRepository.saveFile(newFileDTO);
-            }
-
-            // 파일 변경 없을 경우
-        } else {
-            coachDTO.setFileAttached(existingCoach.getFileAttached());
-        }
+//        List<MultipartFile> newFiles = coachDTO.getFile();
+//        String savePath = "/Users/selimjo/Desktop/letskuf_pic/player/";
+//
+//        // 파일 변경 있을 경우
+//        if (newFiles != null && !newFiles.isEmpty() && !newFiles.get(0).isEmpty()) {
+//            // 기존 파일이 있다면 삭제 처리
+//            List<CoachFileDTO> existingFiles = coachRepository.selectCoachFileListByListByCoachId(coachDTO.getCoachId());
+//
+//            for (CoachFileDTO file : existingFiles) {
+//                File storedFile = new File(savePath + file.getStoredFileName());
+//                if (storedFile.exists()) {
+//                    storedFile.delete();
+//                }
+//                log.info("파일 변경있을 경우 삭제 되어야함");
+//                coachRepository.deleteFile(file.getFileId());
+//            }
+//
+//            // 새 파일 저장
+//            coachDTO.setFileAttached(1);
+//            for (MultipartFile coachFile: newFiles) {
+//                String originalFileName = coachFile.getOriginalFilename();
+//                String storedFileName = System.currentTimeMillis() + "-" + originalFileName;
+//                String storedFilePath = savePath + storedFileName;
+//
+//                // 저장 폴더 없으면 생성
+//                Path directoryPath = Paths.get(savePath);
+//                if (!Files.exists(directoryPath)) {
+//                    Files.createDirectories(directoryPath);
+//                }
+//
+//                coachFile.transferTo(new File(storedFilePath));
+//
+//                CoachFileDTO newFileDTO = new CoachFileDTO();
+//                newFileDTO.setOriginalFileName(originalFileName);
+//                newFileDTO.setStoredFileName(storedFileName);
+//                newFileDTO.setCoachId(coachDTO.getCoachId());
+//
+//                coachRepository.saveFile(newFileDTO);
+//            }
+//
+//            // 파일 변경 없을 경우
+//        } else {
+//            coachDTO.setFileAttached(existingCoach.getFileAttached());
+//        }
         // 선수 정보 수정
         coachRepository.update(coachDTO);
     }
@@ -145,11 +147,11 @@ public class CoachService {
         }
 
         // 파일이 있을 경우 파일도 같이 삭제
-        List<CoachFileDTO> existingCoachFile = coachRepository.selectCoachFileListByListByCoachId(coachId);
-
-        if (existingCoachFile != null && !existingCoachFile.isEmpty()) {
-            coachRepository.deleteFile(existingCoachFile.get(0).getFileId());
-        }
+//        List<CoachFileDTO> existingCoachFile = coachRepository.selectCoachFileListByListByCoachId(coachId);
+//
+//        if (existingCoachFile != null && !existingCoachFile.isEmpty()) {
+//            coachRepository.deleteFile(existingCoachFile.get(0).getFileId());
+//        }
 
         coachRepository.delete(coachId);
     }
@@ -177,10 +179,10 @@ public class CoachService {
         CoachDTO coach = coachRepository.selectUpdateCoachById(coachId);
 
         // 코칭스태프/임원 사진 정보
-        CoachFileDTO coachFile = coachRepository.selectUpdateCoachFileById(coachId);
+        //CoachFileDTO coachFile = coachRepository.selectUpdateCoachFileById(coachId);
 
         map.put("coach", coach);
-        map.put("file", coachFile);
+        //map.put("file", coachFile);
 
         return map;
     }
--- src/main/java/com/letskuf/service/PlayerService.java
+++ src/main/java/com/letskuf/service/PlayerService.java
@@ -30,11 +30,11 @@ public class PlayerService {
     public void registerPlayer(PlayerDTO playerDTO) throws IOException {
 
         // 파일 여부 체크
-        if (playerDTO.getFile().get(0).isEmpty()) {
-            playerDTO.setFileAttached(0);
-        } else {
-            playerDTO.setFileAttached(1);
-        }
+//        if (playerDTO.getFile().get(0).isEmpty()) {
+//            playerDTO.setFileAttached(0);
+//        } else {
+//            playerDTO.setFileAttached(1);
+//        }
 
         // player 저장
         playerRepository.save(playerDTO);
@@ -43,41 +43,41 @@ public class PlayerService {
         playerRepository.savePlayerTeam(playerDTO);
 
         // 파일 저장
-        if (playerDTO.getFile().get(0).isEmpty()) {
-            return;
-        }
+//        if (playerDTO.getFile().get(0).isEmpty()) {
+//            return;
+//        }
 
         // 파일만 따로 가져오기
-        for (MultipartFile playerFile: playerDTO.getFile()) {
-            // 파일 이름 가져오기
-            String originalFileName = playerFile.getOriginalFilename();
-            // 저장용 이름 만들기
-            String storedFileName = System.currentTimeMillis() + "-" + originalFileName;
-            // PlayerFileDTO 세팅 (originalFileName, storedFileName, playerId)
-            PlayerFileDTO playerFileDTO = new PlayerFileDTO();
-            playerFileDTO.setOriginalFileName(originalFileName);
-            playerFileDTO.setStoredFileName(storedFileName);
-            playerFileDTO.setPlayerId(playerDTO.getPlayerId());
-            // 파일 저장용 폴더에 파일 저장 처리
-            String savePath = "/Users/selimjo/Desktop/letskuf_pic/player/";
-            String storedFilePath = savePath + storedFileName;
-
-            // 파일 저장용 폴더 없는 경우 폴더 생성
-            Path directoryPath = Paths.get(savePath);
-            if (!Files.exists(directoryPath)) {
-                try {
-                    Files.createDirectories(directoryPath);
-                    log.info("폴더 생성 완료: " + savePath);
-                } catch (IOException e) {
-                    log.error("파일 저장 폴더 생성 실패: " + savePath, e);
-                    throw new IOException("파일 저장 폴더 생성에 실패하였습니다.", e);
-                }
-            }
-
-            playerFile.transferTo(new File(storedFilePath));
-            // player_file 테이블에 저장
-            playerRepository.saveFile(playerFileDTO);
-        }
+//        for (MultipartFile playerFile: playerDTO.getFile()) {
+//            // 파일 이름 가져오기
+//            String originalFileName = playerFile.getOriginalFilename();
+//            // 저장용 이름 만들기
+//            String storedFileName = System.currentTimeMillis() + "-" + originalFileName;
+//            // PlayerFileDTO 세팅 (originalFileName, storedFileName, playerId)
+//            PlayerFileDTO playerFileDTO = new PlayerFileDTO();
+//            playerFileDTO.setOriginalFileName(originalFileName);
+//            playerFileDTO.setStoredFileName(storedFileName);
+//            playerFileDTO.setPlayerId(playerDTO.getPlayerId());
+//            // 파일 저장용 폴더에 파일 저장 처리
+//            String savePath = "/Users/selimjo/Desktop/letskuf_pic/player/";
+//            String storedFilePath = savePath + storedFileName;
+//
+//            // 파일 저장용 폴더 없는 경우 폴더 생성
+//            Path directoryPath = Paths.get(savePath);
+//            if (!Files.exists(directoryPath)) {
+//                try {
+//                    Files.createDirectories(directoryPath);
+//                    log.info("폴더 생성 완료: " + savePath);
+//                } catch (IOException e) {
+//                    log.error("파일 저장 폴더 생성 실패: " + savePath, e);
+//                    throw new IOException("파일 저장 폴더 생성에 실패하였습니다.", e);
+//                }
+//            }
+//
+//            playerFile.transferTo(new File(storedFilePath));
+//            // player_file 테이블에 저장
+//            playerRepository.saveFile(playerFileDTO);
+//        }
     }
 
     /* 선수 수정 */
@@ -91,50 +91,50 @@ public class PlayerService {
         }
 
         // 파일이 새로 업로드 되었는지 확인
-        List<MultipartFile> newFiles = playerDTO.getFile();
-        String savePath = "/Users/selimjo/Desktop/letskuf_pic/player/";
+//        List<MultipartFile> newFiles = playerDTO.getFile();
+//        String savePath = "/Users/selimjo/Desktop/letskuf_pic/player/";
 
         // 파일 변경 있을 경우
-        if (newFiles != null && !newFiles.isEmpty() && !newFiles.get(0).isEmpty()) {
-            // 기존 파일이 있다면 삭제 처리
-            List<PlayerFileDTO> existingFiles = playerRepository.selectPlayerFileListByPlayerId(playerDTO.getPlayerId());
-
-            for (PlayerFileDTO file : existingFiles) {
-                File storedFile = new File(savePath + file.getStoredFileName());
-                if (storedFile.exists()) {
-                    storedFile.delete();
-                }
-                log.info("파일 변경있을 경우 삭제 되어야함");
-                playerRepository.deleteFile(file.getFileId());
-            }
-
-            // 새 파일 저장
-            playerDTO.setFileAttached(1);
-            for (MultipartFile playerFile: newFiles) {
-                String originalFileName = playerFile.getOriginalFilename();
-                String storedFileName = System.currentTimeMillis() + "-" + originalFileName;
-                String storedFilePath = savePath + storedFileName;
-
-                // 저장 폴더 없으면 생성
-                Path directoryPath = Paths.get(savePath);
-                if (!Files.exists(directoryPath)) {
-                    Files.createDirectories(directoryPath);
-                }
-
-                playerFile.transferTo(new File(storedFilePath));
-
-                PlayerFileDTO newFileDTO = new PlayerFileDTO();
-                newFileDTO.setOriginalFileName(originalFileName);
-                newFileDTO.setStoredFileName(storedFileName);
-                newFileDTO.setPlayerId(playerDTO.getPlayerId());
-
-                playerRepository.saveFile(newFileDTO);
-            }
-
-        // 파일 변경 없을 경우
-        } else {
-            playerDTO.setFileAttached(existingPlayer.getFileAttached());
-        }
+//        if (newFiles != null && !newFiles.isEmpty() && !newFiles.get(0).isEmpty()) {
+//            // 기존 파일이 있다면 삭제 처리
+//            List<PlayerFileDTO> existingFiles = playerRepository.selectPlayerFileListByPlayerId(playerDTO.getPlayerId());
+//
+//            for (PlayerFileDTO file : existingFiles) {
+//                File storedFile = new File(savePath + file.getStoredFileName());
+//                if (storedFile.exists()) {
+//                    storedFile.delete();
+//                }
+//                log.info("파일 변경있을 경우 삭제 되어야함");
+//                playerRepository.deleteFile(file.getFileId());
+//            }
+//
+//            // 새 파일 저장
+//            playerDTO.setFileAttached(1);
+//            for (MultipartFile playerFile: newFiles) {
+//                String originalFileName = playerFile.getOriginalFilename();
+//                String storedFileName = System.currentTimeMillis() + "-" + originalFileName;
+//                String storedFilePath = savePath + storedFileName;
+//
+//                // 저장 폴더 없으면 생성
+//                Path directoryPath = Paths.get(savePath);
+//                if (!Files.exists(directoryPath)) {
+//                    Files.createDirectories(directoryPath);
+//                }
+//
+//                playerFile.transferTo(new File(storedFilePath));
+//
+//                PlayerFileDTO newFileDTO = new PlayerFileDTO();
+//                newFileDTO.setOriginalFileName(originalFileName);
+//                newFileDTO.setStoredFileName(storedFileName);
+//                newFileDTO.setPlayerId(playerDTO.getPlayerId());
+//
+//                playerRepository.saveFile(newFileDTO);
+//            }
+//
+//        // 파일 변경 없을 경우
+//        } else {
+//            playerDTO.setFileAttached(existingPlayer.getFileAttached());
+//        }
         // 선수 정보 수정
         playerRepository.update(playerDTO);
         playerRepository.updatePlayerTeam(playerDTO);
@@ -150,12 +150,12 @@ public class PlayerService {
             throw new IllegalArgumentException("존재하지 않는 선수 입니다.: " + playerId);
         }
 
-        // 파일이 있을 경우 파일도 같이 삭3제
-        List<PlayerFileDTO> existingPlayerFile = playerRepository.selectPlayerFileListByPlayerId(playerId);
-
-        if (existingPlayerFile != null && !existingPlayerFile.isEmpty()) {
-            playerRepository.deleteFile(existingPlayerFile.get(0).getFileId());
-        }
+        // 파일이 있을 경우 파일도 같이 삭제
+//        List<PlayerFileDTO> existingPlayerFile = playerRepository.selectPlayerFileListByPlayerId(playerId);
+//
+//        if (existingPlayerFile != null && !existingPlayerFile.isEmpty()) {
+//            playerRepository.deleteFile(existingPlayerFile.get(0).getFileId());
+//        }
 
         playerRepository.delete(playerId);
         playerRepository.deletePlayerTeam(playerId);
@@ -184,10 +184,10 @@ public class PlayerService {
         PlayerDTO player = playerRepository.selectUpdatePlayerById(playerId);
 
         // 선수 사진 정보
-        PlayerFileDTO playerFile = playerRepository.selectUpdatePlayerFileById(playerId);
+        //PlayerFileDTO playerFile = playerRepository.selectUpdatePlayerFileById(playerId);
 
         map.put("player", player);
-        map.put("file", playerFile);
+        //map.put("file", playerFile);
 
         return map;
     }
